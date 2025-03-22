52de26fcaccd63f6264fb156658b18c14ecfeb86
Balthazar <balthazar.muhlstein@etu.univ-lyon1.fr>
2025-03-22 22:59:17 +0100

    PerformanceRoute : GET

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,20 @@ import express from 'express';
 import connectDB from './db';
 import userRoutes from "./routes/userRoutes"
 import gameRoutes from "./routes/gameRoutes"
+import performanceRoutes from "./routes/performanceRoutes"
 
 const app = express();
 app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/games', gameRoutes);
+app.use('/api/performances', performanceRoutes);
 connectDB(); 
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello, Express with TypeScript!');
 });
-
+ 
 app.listen(PORT, () => {
     console.log(`Serveur démarré sur http://localhost:${PORT}`);
 });
---
+++ src/routes/performanceRoutes.ts
@@ -0,0 +1,43 @@
+import express from 'express';
+import Performance from '../models/Performance';
+const router = express.Router();
+
+
+router.get('/', async (req, res) => {
+  try {
+      const performances = await Performance.find();
+      res.json(performances);
+  } catch (error) {
+      res.status(500).json({ error: error instanceof Error ? error.message : 'Erreur inconnue' });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const performance = await Performance.findById(req.params.id).populate("player game");
+    res.json(performance);
+  } catch (error) {
+      res.status(500).json({ error: error instanceof Error ? error.message : 'Erreur inconnue' });
+  }
+});
+
+router.get('/user/:userId', async (req, res) => {
+  try {
+    const{userId}= req.params;
+    if(!userId) {
+      res.status(400).json({error: "l'ID de l'utilisateur est requis"});
+    }
+
+    //Populate permet de chercher toute la game, pas juste l'ID
+    const performances = await Performance.find({player:userId}).populate('game')
+    if(!performances.length) {
+      res.status(404).json({error:"Aucune partie trouvée pour l'utilisateur"})
+    }
+    res.json(performances);
+  }
+  catch (error) {
+    res.status(500).json({ error: (error as Error).message });
+  }
+})
+
+export default router;
