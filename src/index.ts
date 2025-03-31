6443e7e4a34c173654230a4bb49a20f4c87c0099
Balthazar <balthazar.muhlstein@etu.univ-lyon1.fr>
2025-03-31 08:45:07 +0200

    middleware cors

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,15 @@ import userRoutes from "./routes/userRoutes"
 import gameRoutes from "./routes/gameRoutes"
 import performanceRoutes from "./routes/performanceRoutes"
 import loginRoutes from "./routes/loginRoutes"
-
+import cors from 'cors';
 import { Server } from 'socket.io';
 import http from 'http';
 
 const app = express();
-
+app.use(cors({
+  origin: 'http://localhost:5173', // Remplace par l'URL de ton frontend
+  credentials: true, // Si tu utilises des cookies ou des headers sécurisés
+}));
 app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/games', gameRoutes);
